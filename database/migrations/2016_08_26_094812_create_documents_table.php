333ca4e7b7df3bf049985ce8311b979b95f1490c
Dan Michael O. Heggø <danmichaelo@gmail.com>
2016-10-30 20:01:03 +0100

    fix: Remove length limits from documents table
    With Postgres, there shouldn't be a performance drawback of using text over varchar.

--- database/migrations/2016_08_26_094812_create_documents_table.php
+++ database/migrations/2016_08_26_094812_create_documents_table.php
@@ -22,72 +22,72 @@ class CreateDocumentsTable extends Migration
             $table->date(Document::RECEIVING_OR_ACTIVATION_DATE)->nullable();
 
             // PO Line
-            $table->string(Document::PO_ID)->nullable()->unique();
+            $table->string(Document::PO_ID, 50)->nullable()->unique();
 
-            $table->string('acquisition_method')->nullable();
-            $table->string('additional_order_reference')->nullable();
-            $table->string('reporting_code')->nullable();
-            $table->string('receiving_note')->nullable();
-            $table->string('receiving_status')->nullable();
-            $table->string('cancellation_reason')->nullable();
-            $table->string('vendor_code')->nullable();
-            $table->string('source_type')->nullable();
-            $table->string('fund_ledger_code')->nullable();
-            $table->string('fund_ledger_name')->nullable();
-            $table->string('fund_type')->nullable();
-            $table->string('order_line_type_code')->nullable();
-            $table->string('order_line_type')->nullable();
+            $table->text('acquisition_method')->nullable();
+            $table->text('additional_order_reference')->nullable();
+            $table->text('reporting_code')->nullable();
+            $table->text('receiving_note')->nullable();
+            $table->text('receiving_status')->nullable();
+            $table->text('cancellation_reason')->nullable();
+            $table->text('vendor_code')->nullable();
+            $table->text('source_type')->nullable();
+            $table->text('fund_ledger_code')->nullable();
+            $table->text('fund_ledger_name')->nullable();
+            $table->text('fund_type')->nullable();
+            $table->text('order_line_type_code')->nullable();
+            $table->text('order_line_type')->nullable();
 
-            $table->string('po_creator')->nullable();
-            $table->string('po_modified_by')->nullable();
+            $table->text('po_creator')->nullable();
+            $table->text('po_modified_by')->nullable();
             $table->dateTime('po_creation_date')->nullable();
             $table->dateTime('po_modification_date')->nullable();
             $table->date('sent_date')->nullable();
 
             // Bibliographic data
-            $table->string(Document::MMS_ID)->nullable();
+            $table->text(Document::MMS_ID)->nullable();
             $table->text('title')->nullable();
-            $table->string('edition')->nullable();
-            $table->string('author')->nullable();
-            $table->string('publisher')->nullable();
-            $table->string('publication_date')->nullable();
-            $table->string('publication_place')->nullable();
-            $table->string('series')->nullable();
-            $table->string('bibliographic_level')->nullable();
-            $table->string('dewey_classification')->nullable();          // Should possibly be multi-valued
-            $table->string('dewey_classification_top_line')->nullable();  // Should possibly be multi-valued
-            $table->string('isbn')->nullable();         // Should possibly be multi-valued
-            $table->string('base_status')->nullable();
-            $table->string('material_type')->nullable();
-            $table->string('biblio_modifiedby')->nullable();
+            $table->text('edition')->nullable();
+            $table->text('author')->nullable();
+            $table->text('publisher')->nullable();
+            $table->text('publication_date')->nullable();
+            $table->text('publication_place')->nullable();
+            $table->text('series')->nullable();
+            $table->text('bibliographic_level')->nullable();
+            $table->text('dewey_classification')->nullable();          // Should possibly be multi-valued
+            $table->text('dewey_classification_top_line')->nullable();  // Should possibly be multi-valued
+            $table->text('isbn')->nullable();         // Should possibly be multi-valued
+            $table->text('base_status')->nullable();
+            $table->text('material_type')->nullable();
+            $table->text('biblio_modifiedby')->nullable();
             $table->dateTime('biblio_modified')->nullable();
 
             // Physical holding
-            $table->string('holding_id')->nullable();
-            $table->string('library_name')->nullable();
-            $table->string('location_name')->nullable();
-            $table->string('permanent_call_number')->nullable();
-            $table->string('temporary_library_code')->nullable();
-            $table->string('temporary_library_name')->nullable();
-            $table->string('temporary_location_name')->nullable();
+            $table->text('holding_id')->nullable();
+            $table->text('library_name')->nullable();
+            $table->text('location_name')->nullable();
+            $table->text('permanent_call_number')->nullable();
+            $table->text('temporary_library_code')->nullable();
+            $table->text('temporary_library_name')->nullable();
+            $table->text('temporary_location_name')->nullable();
 
             // Physical item
-            $table->string('item_id')->nullable();
-            $table->string('item_policy')->nullable();
-            $table->string('barcode')->nullable();
-            $table->string('item_creator')->nullable();
-            $table->string('process_type')->nullable();
+            $table->text('item_id')->nullable();
+            $table->text('item_policy')->nullable();
+            $table->text('barcode')->nullable();
+            $table->text('item_creator')->nullable();
+            $table->text('process_type')->nullable();
             $table->dateTime('item_creation_date')->nullable();
             $table->dateTime('receiving_date')->nullable();
 
             // Electronic portfolio
-            $table->string('portfolio_id')->nullable();
-            $table->string('collection_name')->nullable();
+            $table->text('portfolio_id')->nullable();
+            $table->text('collection_name')->nullable();
             $table->date('portfolio_creation_date')->nullable();
             $table->date('activation_date')->nullable();
 
             // Enrichments
-            $table->string('cover_image')->nullable();
+            $table->text('cover_image')->nullable();
             $table->text('description')->nullable();
         });
     }
