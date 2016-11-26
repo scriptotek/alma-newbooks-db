bc6443ee39ecd1de1a8b36a4e4a1bd92e7a7fa03
Dan Michael O. Heggø <danmichaelo@gmail.com>
2016-11-26 11:38:35 +0100

    feat: Store changes

---
+++ database/migrations/2016_11_26_093145_create_changes_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateChangesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('changes', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('document_id')->unsigned();
+            $table->text('key');
+            $table->text('old_value');
+            $table->text('new_value');
+            $table->dateTime('created_at')->useCurrent();
+
+            $table->foreign('document_id')->references('id')->on('documents');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('changes');
+    }
+}
