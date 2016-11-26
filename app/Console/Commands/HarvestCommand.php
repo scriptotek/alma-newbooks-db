8372ffd923c9a1a82a7b5ee02b78391dbc85bbd6
Dan Michael O. Heggø <danmichaelo@gmail.com>
2016-11-26 20:37:58 +0100

    Show log messages on console

--- app/Console/Commands/HarvestCommand.php
+++ app/Console/Commands/HarvestCommand.php
@@ -27,9 +27,14 @@ class HarvestCommand extends Command
      */
     public function handle()
     {
+        \Event::listen('illuminate.log', function($level, $msg, $extras)
+        {
+            $msg = $msg . ' ' . json_encode($extras);
+            $this->line($msg, $level);
+        });
+
         $days = $this->argument('days');
-        \Log::info("Starting harvest: Fetching last {$days} days of records");
-        $this->info("Dispatching jobs for harvesting the last {$days} days of records");
+        \Log::info("Dispatching jobs to harvest the last {$days} days of records");
 
         dispatch(new HarvestPrintBooksReport($days));
         dispatch(new HarvestEBooksReport($days));
