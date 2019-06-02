9e4fd39cd21356d6d9ba63e7ae01c98a4b6cca96
Dan Michael O. Heggø <danmichaelo@gmail.com>
2019-06-02 13:29:34 +0200

    Fix: Remove bootstrap-sass reference

--- webpack.mix.js
+++ webpack.mix.js
@@ -12,8 +12,7 @@ let mix = require('laravel-mix');
  */
 
 mix.js('resources/assets/js/app.js', 'public/js')
-   .sass('resources/assets/sass/app.scss', 'public/css')
-   .copy('node_modules/bootstrap-sass/assets/fonts/bootstrap/', 'public/fonts/bootstrap');
+   .sass('resources/assets/sass/app.scss', 'public/css');
 
 if (mix.inProduction()) {
     mix.version();
